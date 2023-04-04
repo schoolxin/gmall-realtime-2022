f57475b423de2649c6d8c95f0e00bf74ab07a729
dengzz <707643733@qq.com>
2023-04-04 23:24:43 +0800

    .

--- gamll_realtime/src/test/java/Flink06_LookUp_JoinTest.java
+++ gamll_realtime/src/test/java/Flink06_LookUp_JoinTest.java
@@ -8,7 +8,7 @@ import static org.apache.flink.table.api.Expressions.$;
 
 public class Flink06_LookUp_JoinTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -27,42 +27,43 @@ public class Flink06_LookUp_JoinTest {
                 "  'url' = 'jdbc:mysql://hadoop102:3306/gmall', " +
                 "  'table-name' = 'base_dic', " +
                 "  'driver' = 'com.mysql.cj.jdbc.Driver', " +
-                "  'lookup.cache.max-rows' = '10', " +  //维表数据不变or会改变,但是数据的准确度要求不高
-                "  'lookup.cache.ttl' = '1 hour', " +
+                "  'lookup.cache.max-rows' = '10', " +  //维表数据不变or会改变,但是数据的准确度要求不高 缓存10条
+                "  'lookup.cache.ttl' = '1 hour', " + //这个和上面的最大行数 必须同时写
                 "  'username' = 'root', " +
                 "  'password' = '000000' " +
                 ")");
 
         //打印LookUp表
-        tableEnv.sqlQuery("select * from base_dic")
-                .execute()
-                .print();
+//        tableEnv.sqlQuery("select * from base_dic")
+//                .execute()
+//                .print();
 
-        //构建事实表
-//        SingleOutputStreamOperator<WaterSensor> waterSensorDS = env.socketTextStream("hadoop102", 8888)
-//                .map(line -> {
-//                    String[] split = line.split(",");
-//                    return new WaterSensor(split[0],
-//                            Double.parseDouble(split[1]),
-//                            Long.parseLong(split[2]));
-//                });
-//        Table table = tableEnv.fromDataStream(waterSensorDS,
-//                $("id"),
-//                $("vc"),
-//                $("ts"),
-//                $("pt").proctime());
-//        tableEnv.createTemporaryView("t1", table);
-//
-//        //使用事实表关联维表并打印结果
-//        tableEnv.sqlQuery("" +
-//                "select " +
-//                "    t1.id, " +
-//                "    t1.vc, " +
-//                "    dic.dic_name " +
-//                "from t1 " +
-//                "join base_dic FOR SYSTEM_TIME AS OF t1.pt as dic " +
-//                "on t1.id=dic.dic_code")
-//                .execute().print();
+//        构建事实表
+        SingleOutputStreamOperator<WaterSensor> waterSensorDS = env.socketTextStream("hadoop102", 8888)
+                .map(line -> {
+                    String[] split = line.split(",");
+                    return new WaterSensor(split[0],
+                            Double.parseDouble(split[1]),
+                            Long.parseLong(split[2]));
+                });
+
+        Table table = tableEnv.fromDataStream(waterSensorDS,
+                $("id"),
+                $("vc"),
+                $("ts"),
+                $("pt").proctime());
+        tableEnv.createTemporaryView("t1", table);
+
+        //使用事实表关联维表并打印结果
+        tableEnv.sqlQuery("" +
+                        "select " +
+                        "    t1.id, " +
+                        "    t1.vc, " +
+                        "    dic.dic_name " +
+                        "from t1 " +
+                        "join base_dic FOR SYSTEM_TIME AS OF t1.pt as dic " +
+                        "on t1.id=dic.dic_code")
+                .execute().print();
 
     }
 
